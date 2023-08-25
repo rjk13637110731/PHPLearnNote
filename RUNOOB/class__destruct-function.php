e08018f8f3e64dedd18f11cb0dc576b5d5b7b244
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-25 23:22:48 +0800

    Create class__destruct-function.php

---
+++ RUNOOB/class__destruct-function.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        class Site{
+            var $url;
+            var $title;
+
+            function __construct($par1,$par2){
+                $this->url = $par1;
+                $this->title = $par2;
+                $this->name = "Site";
+                print "构造函数<br>";
+            }
+
+            function __destruct(){
+                print "销毁".$this->name."析构函数执行<br>";
+            }
+
+            function  setUrl($par){
+                $this->url = $par;
+            }
+
+            function getUrl(){
+                echo $this->url."<br>";
+            }
+
+            function setTitle($par){
+                $this->title = $par;
+            }
+
+            function getTitle(){
+                echo $this->title."<br>";
+            }
+        }
+
+        $runoobnew = new Site("www.runoob.com.new","菜鸟教程new");
+        $taobaonew = new Site("www.taobao.com.new","淘宝网站new");
+        $googlenew = new Site("www.google.com.new","谷歌网站new");
+
+        $runoobnew->getTitle();
+        $taobaonew->getTitle();
+        $googlenew->getTitle();
+
+        $runoobnew->getUrl();
+        $taobaonew->getUrl();
+        $googlenew->getUrl();
+
+
+    ?>
+</body>
+</html>
