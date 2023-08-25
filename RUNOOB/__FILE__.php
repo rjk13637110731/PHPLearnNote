0c4f05430880d424247cc3d7dbc85d1a9c78f87e
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-25 15:32:18 +0800

    Create __FILE__.php

---
+++ RUNOOB/__FILE__.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '该文件位于 "'.__FILE__.'"';
+    ?>
+</body>
+</html>
