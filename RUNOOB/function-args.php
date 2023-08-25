55de14c547a2d3b4f30787e21d4d3f00c4cc8b56
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-25 15:15:56 +0800

    Create function-args.php

---
+++ RUNOOB/function-args.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function writeName($fname){
+            echo $fname." Bateler.<br>";
+        }
+
+        echo "My name is ";
+        writeName("Kai Jim");
+
+        echo "My sister's name is ";
+        writeName("Hege");
+
+        echo "My brother's name is ";
+        writeName("Stale");
+    ?>
+</body>
+</html>
